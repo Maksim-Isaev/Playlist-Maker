7024aa04096940c67b2a3d0be515807086ecd6ea
Broiler 747 <max.isaev16@gmail.com>
2024-04-04 22:09:51 +0300

    Sprint 8. Homework 2. Clicking on the buttons on the main screen is implemented using various methods. The first button uses an anonymous class to implement its functionality, the second uses a lambda expression, and the third also uses an anonymous class.

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.practicum.playlistmaker
-
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val button1 = findViewById<Button>(R.id.button1)
+        val button2 = findViewById<Button>(R.id.button2)
+        val button3 = findViewById<Button>(R.id.button3)
+
+        // Реализация нажатия на первую кнопку через анонимный класс
+        button1.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Button 1 - Анонимный класс", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        // Реализация нажатия на вторую кнопку через лямбда-выражение
+        button2.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Button 2 - Лямбда-выражение", Toast.LENGTH_SHORT).show()
         }
+
+        // Реализация нажатия на третью кнопку через анонимный класс
+        button3.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Button 3 - Анонимный класс", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
