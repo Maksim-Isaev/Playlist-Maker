79de5b24b080df41e7d7d4198f82f6a5e4cd5b1b
Isaev Maksim <m.isaev96@yandex.ru>
2024-04-15 15:26:22 +0300

    Sprint 9. Homework 1 - completed

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,17 +1,29 @@
 package com.practicum.playlistmaker
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 import androidx.appcompat.widget.Toolbar
 import com.practicum.playlistmaker.R
 
 class SettingsActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId", "WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
         val backButton = findViewById<Toolbar>(R.id.toolbar)
-        backButton.setOnClickListener{
+        backButton.setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
+        val swDarkTheme = findViewById<SwitchCompat>(R.id.night_Mode)
+        swDarkTheme.setOnClickListener {
+            if (swDarkTheme.isChecked)
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            else
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+
     }
 }
