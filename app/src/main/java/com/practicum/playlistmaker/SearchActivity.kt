c46fa2227945fba9a9dd708e94ae60842d5f7cdb
Isaev Maksim <m.isaev96@yandex.ru>
2024-05-25 22:29:39 +0300

    Sprint 11. HW. 1. Обновлен SearchActivity.kt для работы с api - api пока не работает - ищу проблему 2. обновлен activity_search.xml так как в textview сидела ошибка которая приводила к вылету приложения при открытии окна поиск

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -6,19 +6,45 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.MotionEvent
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.practicum.playlistmaker.api.ItunesResponse
+import com.practicum.playlistmaker.api.ResultResponse
+import com.practicum.playlistmaker.api.api
 import com.practicum.playlistmaker.recycleView.TrackAdapter
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class SearchActivity : AppCompatActivity() {
     private var clearButtonVisibility = false
     private var searchValue = TEXT_DEF
+    private val baseUrl = "https://itunes.apple.com/"
     private lateinit var adapter: TrackAdapter
+    private val tracks = ArrayList<Track>()
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var placeholderMessage: TextView
+    private lateinit var placeholderImage: ImageView
+    private lateinit var updateButton: Button
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+    private val iTunesService = retrofit.create(api::class.java)
 
     companion object {
         const val SEARCH_TEXT = "SEARCH_TEXT"
@@ -38,11 +64,17 @@ class SearchActivity : AppCompatActivity() {
         }
 
         // Реализация поиска
+        val cancelBtn = findViewById<ImageView>(R.id.cancel_button)
         val searchBar = findViewById<EditText>(R.id.search_bar)
         searchBar.requestFocus() // Запрос фокуса на строку поиска
         searchBar.hint = getString(R.string.search) // Установка подсказки для строки поиска
         searchBar.setText(searchValue) // Установка текущего значения поиска в строку поиска
-
+        cancelBtn.setOnClickListener {
+            searchBar.text.clear()
+            tracks.clear()
+            adapter.notifyDataSetChanged()
+            showMessage("", "", ResultResponse.SUCCESS)
+        }
         // TextWatcher отслеживает изменения в EditText
         val simpleTextWatcher = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -77,10 +109,14 @@ class SearchActivity : AppCompatActivity() {
         }
         val recyclerView = findViewById<RecyclerView>(R.id.recycle_view)
         recyclerView.layoutManager = LinearLayoutManager(this)
-        adapter = TrackAdapter(createList())
+        adapter = TrackAdapter()
         recyclerView.adapter = adapter
-
-
+        placeholderImage = findViewById(R.id.placeholderImage)
+        placeholderMessage = findViewById(R.id.placeholderMessage)
+        updateButton = findViewById(R.id.updateResponse)
+        updateButton.setOnClickListener {
+            search()
+        }
     }
 
     // Сохраняем текущее значение поиска в Bundle outState
@@ -112,41 +148,87 @@ class SearchActivity : AppCompatActivity() {
             true
         }
     }
+    private fun search() {
+        iTunesService.search(searchValue)
+            .enqueue(object : Callback<ItunesResponse> {
+                override fun onResponse(
+                    call: Call<ItunesResponse>,
+                    response: Response<ItunesResponse>,
+                ) {
+                    when (response.code()) {
+                        200 -> {
+                            if (response.body()?.results?.isNotEmpty() == true) {
+                                tracks.clear()
+                                tracks.addAll(response.body()?.results!!)
+                                adapter.notifyDataSetChanged()
+                                showMessage("", "", ResultResponse.SUCCESS)
+                            } else {
+                                showMessage(
+                                    getString(R.string.nothing),
+                                    "",
+                                    ResultResponse.EMPTY
+                                )
+                            }
+                        }
 
+                        else -> showMessage(
+                            getString(R.string.connect_error),
+                            response.code().toString(),
+                            ResultResponse.ERROR
+                        )
+                    }
+                }
 
-    fun createList(): List<Track> {
-        return listOf(
-            Track(
-                "Smells Like Teen Spirit",
-                "Nirvana",
-                "5:01",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-            ),
-            Track(
-                "Billie Jean",
-                "Michael Jackson",
-                "4:35",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
-            ),
-            Track(
-                "Stayin' Alive",
-                "Bee Gees",
-                "4:10",
-                "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
-            ),
-            Track(
-                "Whole Lotta Love",
-                "Led Zeppelin",
-                "5:33",
-                "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
-            ),
-            Track(
-                "Sweet Child O'Mine",
-                "Guns N' Roses",
-                "5:03",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
-            )
-        )
+                override fun onFailure(call: Call<ItunesResponse>, t: Throwable) {
+                    showMessage(
+                        getString(R.string.connect_error),
+                        t.message.toString(),
+                        ResultResponse.ERROR
+                    )
+                }
+
+            })
+    }
+
+    private fun showMessage(text: String, additionalMessage: String, errorType: ResultResponse) {
+        when (errorType) {
+            ResultResponse.SUCCESS -> {
+                placeholderMessage.visibility = GONE
+                placeholderImage.visibility = GONE
+                recyclerView.visibility = VISIBLE
+                updateButton.visibility = GONE
+            }
+
+            ResultResponse.EMPTY -> {
+                recyclerView.visibility = GONE
+                placeholderMessage.visibility = VISIBLE
+                placeholderImage.visibility = VISIBLE
+                updateButton.visibility = GONE
+                tracks.clear()
+                adapter.notifyDataSetChanged()
+                placeholderMessage.text = text
+                placeholderImage.setImageResource(R.drawable.nothing)
+                if (additionalMessage.isNotEmpty()) {
+                    Toast.makeText(applicationContext, additionalMessage, Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
+
+            ResultResponse.ERROR -> {
+                recyclerView.visibility = GONE
+                placeholderMessage.visibility = VISIBLE
+                placeholderImage.visibility = VISIBLE
+                updateButton.visibility = VISIBLE
+                tracks.clear()
+                adapter.notifyDataSetChanged()
+                placeholderMessage.text = text
+                placeholderImage.setImageResource(R.drawable.connect_error)
+                if (additionalMessage.isNotEmpty()) {
+                    Toast.makeText(applicationContext, additionalMessage, Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
+        }
     }
 
 }
