84deb1565f9532196e4c096ec87815a535ec4122
Isaev Maksim <m.isaev96@yandex.ru>
2024-05-30 21:43:19 +0300

    поправка

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,12 +1,14 @@
 package com.practicum.playlistmaker
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
