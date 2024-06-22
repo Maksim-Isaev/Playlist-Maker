ebee7e3fa81f88b6f7c481c171671b9fb26c27e0
Isaev Maksim <m.isaev96@yandex.ru>
2024-06-22 19:07:40 +0300

    HW 13. Скорректирован Track.kt под новые данные

--- app/src/main/java/com/practicum/playlistmaker/Track.kt
+++ app/src/main/java/com/practicum/playlistmaker/Track.kt
@@ -1,9 +1,22 @@
 package com.practicum.playlistmaker
 
+import java.io.Serializable
+import java.text.SimpleDateFormat
+import java.util.Locale
+
 data class Track(
     val trackId: String,
     val trackName: String,      //Track Name
     val artistName: String,     //Artist Name
     val trackTimeMillis: Long,  //Track duration
-    val artworkUrl100: String   //Link to the cover image
-)
+    val artworkUrl100: String,   //Link to the cover image
+    val collectionName: String,
+    val releaseDate: String = "",
+    val primaryGenreName: String = "",
+    val country: String = ""
+) : Serializable {
+    val artworkUrl512: String
+    get() = artworkUrl100.replaceAfterLast('/', "512x512bb.jpg")
+    val trackTime: String get() =
+    SimpleDateFormat("mm:ss", Locale.getDefault()).format(trackTimeMillis)
+}
