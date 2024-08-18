ae0a0904fe40d73a80b05cc30190565ebd17b260
Isaev Maksim <m.isaev96@yandex.ru>
2024-08-18 21:37:39 +0300

    HW. 16  Обновлен MainActivity

--- app/src/main/java/com/practicum/playlistmaker/main/ui/main/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/main/ui/main/MainActivity.kt
@@ -3,42 +3,40 @@ package com.practicum.playlistmaker.main.ui.main
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.practicum.playlistmaker.R
+import com.practicum.playlistmaker.databinding.ActivityMainBinding
 import com.practicum.playlistmaker.media.ui.media.MediaActivity
-import com.practicum.playlistmaker.presentation.ui.search.SearchActivity
+import com.practicum.playlistmaker.search.ui.SearchActivity
 import com.practicum.playlistmaker.settings.ui.SettingsActivity
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
-        val buttonSearch = findViewById<Button>(R.id.button_search)
-        val buttonMedia = findViewById<Button>(R.id.button_media)
-        val buttonSettings = findViewById<Button>(R.id.button_settings)
-
-        // Реализация нажатия на первую кнопку (поиск) через лямбда-выражение (скорректированно после 8 спринта)
-        buttonSearch.setOnClickListener{
-            val intent = Intent(this@MainActivity, SearchActivity::class.java)
+        binding.buttonSearch.setOnClickListener {
+            val intent = Intent(this, SearchActivity::class.java)
             startActivity(intent)
         }
 
-        // Реализация нажатия на вторую кнопку (медиатека) через лямбда-выражение
-        buttonMedia.setOnClickListener {
-            //Toast.makeText(this@MainActivity, "Button 2 - Лямбда-выражение", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this@MainActivity, MediaActivity::class.java)
+        val buttonMediaClickListener: View.OnClickListener = View.OnClickListener {
+            val intent = Intent(this, MediaActivity::class.java)
             startActivity(intent)
         }
 
-        // Реализация нажатия на третью кнопку (настройки) через через лямбда-выражение (скорректированно после 8 спринта)
-        buttonSettings.setOnClickListener {
-            //Toast.makeText(this@MainActivity, "Button 2 - Лямбда-выражение", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this@MainActivity, SettingsActivity::class.java)
-            startActivity(intent)
-            }
+        binding.btnLibrary.setOnClickListener(buttonMediaClickListener)
 
+        binding.buttonSettings.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
     }
+}
 }
