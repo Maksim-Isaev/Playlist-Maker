cb1641d355ed1fc13ffa5b43fbee4ab990328754
Isaev Maksim <m.isaev96@yandex.ru>
2024-09-22 21:04:18 +0300

    HW_19.1 - поправлен баг при котором происходил вылет приложения при втором нажатии свича переключения тем

--- app/src/main/java/com/practicum/playlistmaker/media/ui/media/MediaFragment.kt
+++ app/src/main/java/com/practicum/playlistmaker/media/ui/media/MediaFragment.kt
@@ -1,6 +1,5 @@
 package com.practicum.playlistmaker.media.ui.media
 
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -19,27 +18,27 @@ class MediaFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?,
+        savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentMediaBinding.inflate(layoutInflater)
+        _binding = FragmentMediaBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         binding.viewPager.adapter = MediaAdapter(childFragmentManager, lifecycle)
         tabMediator = TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
             when (position) {
                 0 -> tab.text = getString(R.string.favorite_tracks)
-
                 1 -> tab.text = getString(R.string.playlists)
             }
         }
         tabMediator.attach()
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onDestroyView() {
+        super.onDestroyView()
         tabMediator.detach()
         _binding = null
     }
