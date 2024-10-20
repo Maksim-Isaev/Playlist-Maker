220ff6d4c5215c20d8a10382cd66a537e1443b1f
Isaev Maksim <m.isaev96@yandex.ru>
2024-10-20 20:54:57 +0300

    HW 21.Добавлены зависимости для базы данных и реализации FavoritesRepo в mediaModule

--- app/src/main/java/com/practicum/playlistmaker/di/mediaModule.kt
+++ app/src/main/java/com/practicum/playlistmaker/di/mediaModule.kt
@@ -1,16 +1,29 @@
 package com.practicum.playlistmaker.di
 
+import androidx.room.Room
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import com.practicum.playlistmaker.media.ui.media.FavoritesViewModel
 import com.practicum.playlistmaker.media.ui.media.PlaylistViewModel
+import com.practicum.playlistmaker.search.data.db.AppDatabase
+import com.practicum.playlistmaker.search.data.repository.FavoritesRepo
+import com.practicum.playlistmaker.search.data.repository.FavoritesRepoImpl
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.scope.get
 
 val mediaModule = module {
     viewModel {
-        FavoritesViewModel()
+        FavoritesViewModel(get())
     }
 
     viewModel {
         PlaylistViewModel()
     }
+    single {
+        Room.databaseBuilder(androidContext(), AppDatabase::class.java, "play-list-maker-db")
+            .build()
+    }
+
+    single<FavoritesRepo> { FavoritesRepoImpl(get()) }
+
 }
