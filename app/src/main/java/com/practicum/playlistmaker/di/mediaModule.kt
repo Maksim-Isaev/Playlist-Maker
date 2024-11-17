31d36734ab503fc4eca713ac17e1b15ddaf841c8
Isaev Maksim <m.isaev96@yandex.ru>
2024-11-17 19:57:12 +0300

    HW_22 mediaModule

--- app/src/main/java/com/practicum/playlistmaker/di/mediaModule.kt
+++ app/src/main/java/com/practicum/playlistmaker/di/mediaModule.kt
@@ -1,16 +1,23 @@
 package com.practicum.playlistmaker.di
 
 import androidx.room.Room
+import com.practicum.playlistmaker.media.data.converter.PlaylistDBConverter
 import com.practicum.playlistmaker.media.data.converter.TrackDBConverter
 import com.practicum.playlistmaker.media.data.db.AppDatabase
 import com.practicum.playlistmaker.media.data.repository.FavoritesRepositoryImpl
+import com.practicum.playlistmaker.media.data.repository.PlaylistRepositoryImpl
 import com.practicum.playlistmaker.media.domain.api.FavoritesInteractor
 import com.practicum.playlistmaker.media.domain.api.FavoritesRepository
+import com.practicum.playlistmaker.media.domain.api.PlaylistInteractor
+import com.practicum.playlistmaker.media.domain.api.PlaylistRepository
 import com.practicum.playlistmaker.media.domain.impl.FavoritesInteractorImpl
+import com.practicum.playlistmaker.media.domain.impl.PlaylistInteractorImpl
 import com.practicum.playlistmaker.media.ui.FavoritesViewModel
+import com.practicum.playlistmaker.media.ui.NewPlaylistViewModel
 import com.practicum.playlistmaker.media.ui.PlaylistViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.scope.get
 import org.koin.dsl.module
 
 val mediaModule = module {
@@ -19,7 +26,11 @@ val mediaModule = module {
     }
 
     viewModel {
-        PlaylistViewModel()
+        PlaylistViewModel(get())
+    }
+
+    viewModel {
+        NewPlaylistViewModel(get(), get())
     }
     single {
         Room.databaseBuilder(androidContext(), AppDatabase::class.java, "play-list-maker-db")
@@ -28,10 +39,19 @@ val mediaModule = module {
     single {
         TrackDBConverter()
     }
-
+    single {
+        PlaylistDBConverter()
+    }
     single<FavoritesRepository> { FavoritesRepositoryImpl(get(), get()) }
 
     single<FavoritesInteractor> {
         FavoritesInteractorImpl(get())
+    }
+    single<PlaylistRepository> {
+        PlaylistRepositoryImpl(get(), get())
+    }
+
+    single<PlaylistInteractor> {
+        PlaylistInteractorImpl(get())
     }
 }
