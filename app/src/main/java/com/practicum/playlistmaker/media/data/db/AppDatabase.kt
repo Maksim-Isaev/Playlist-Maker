545dc498e898c0d302f08ab1a57184885ecbb86c
Isaev Maksim <m.isaev96@yandex.ru>
2024-11-17 19:52:49 +0300

    HW_22 fragment new play list & DB

--- app/src/main/java/com/practicum/playlistmaker/media/data/db/AppDatabase.kt
+++ app/src/main/java/com/practicum/playlistmaker/media/data/db/AppDatabase.kt
@@ -2,6 +2,8 @@ package com.practicum.playlistmaker.media.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.practicum.playlistmaker.media.data.db.dao.PlaylistDao
+import com.practicum.playlistmaker.media.data.db.dao.TrackDao
 import com.practicum.playlistmaker.media.data.db.entity.PlaylistEntity
 import com.practicum.playlistmaker.media.data.db.entity.TrackEntity
 
